d292f79b27ee1594d2b00b0fb0578fe602af11f2
AquilaXk <illusiveman7@gmail.com>
2026-04-16 18:11:33 +0900

    Initial commit: Setup project

---
+++ src/test/java/com/aquilabank/AquilaBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aquilabank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AquilaBankApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
