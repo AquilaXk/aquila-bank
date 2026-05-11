cf45a4f1a9171455d0172f219eb576174ef91c4d
AquilaXk <illusiveman7@gmail.com>
2026-05-11 17:25:47 +0900

    test: public frontend live smoke 추가

--- tools/test/check-staging-https-env-contract.sh
+++ tools/test/check-staging-https-env-contract.sh
@@ -25,9 +25,13 @@ require_pattern "${workflow_path}" 'NGINX_ENABLE_HTTPS="${NGINX_ENABLE_HTTPS:-au
 require_pattern "${workflow_path}" 'NGINX_SSL_CERTIFICATE_PATH="${NGINX_SSL_CERTIFICATE_PATH:-/etc/letsencrypt/live/${NGINX_SERVER_NAME}/fullchain.pem}"'
 require_pattern "${workflow_path}" 'NGINX_SSL_CERTIFICATE_KEY_PATH="${NGINX_SSL_CERTIFICATE_KEY_PATH:-/etc/letsencrypt/live/${NGINX_SERVER_NAME}/privkey.pem}"'
 require_pattern "${workflow_path}" 'STAGING_PUBLIC_BASE_URL="${STAGING_PUBLIC_BASE_URL:-https://${NGINX_SERVER_NAME}}"'
+require_pattern "${workflow_path}" 'STAGING_PUBLIC_FRONTEND_SMOKE_ENABLED="${STAGING_PUBLIC_FRONTEND_SMOKE_ENABLED:-true}"'
 require_pattern "${workflow_path}" 'PUBLIC_EDGE_BASE_URL="${STAGING_PUBLIC_BASE_URL}"'
 require_pattern "${workflow_path}" 'PUBLIC_EDGE_EXPECT_HSTS=true'
 require_pattern "${workflow_path}" 'tools/test/check-public-edge-bot-guard-live.sh'
+require_pattern "${workflow_path}" 'PUBLIC_FRONTEND_BASE_URL="${STAGING_PUBLIC_BASE_URL}"'
+require_pattern "${workflow_path}" 'PUBLIC_FRONTEND_EXPECT_HSTS=true'
+require_pattern "${workflow_path}" 'tools/test/check-public-frontend-live-smoke.sh'
 
 require_pattern "${deploy_readme_path}" 'NGINX_SERVER_NAME=bank.aquilaxk.site'
 require_pattern "${deploy_readme_path}" 'NGINX_ENABLE_HTTPS=auto'
